7fcbd0823a5ffa71f0725363038d8009379e7703
iuryalves <iuryalves20@gmail.com>
2015-08-19 14:13:25 -0300

    simple setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name="latexmath2png",
+    version="",
+    packages=["latexmath2png"],
+    author="",
+    author_email="",
+    description='',
+    license="",
+    url="",
+)
